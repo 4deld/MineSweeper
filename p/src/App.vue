8ed56f7efd7a69f77a1bd66d22780094c07c2e47
rlapo213 <rlapo@naver.com>
2020-01-30 12:10:16 +0900

    layout

---
+++ p/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+      <!-- <router-link to="/about">About</router-link> -->
+    <router-view/>
+  </div>
+</template>
+
+<style>
+*{
+  margin: 0;
+  box-sizing: border-box;
+  /*user-select: none;*/
+}
+</style>
