3bae1fbeb975525d49d834c5cc0753013a87c5b9
rlapo213 <rlapo@naver.com>
2020-02-02 12:35:38 +0900

    data to style

--- p/src/components/Table.vue
+++ p/src/components/Table.vue
@@ -1,20 +1,114 @@
 <template>
-  <div>
-      <tr v-for="(rowData, rowidx) in tabledata":key rowidx>
-          <td v-for="(colData, colidx) in rowData":key colidx>
-              {{colData}}
+  <table>
+      <tr v-for="(rowData, rowidx) in tabledata" :key="rowidx">
+          <td
+        v-for="(colData, colidx) in rowData"
+        :key="colidx"
+        :style="colDataStyle(rowidx, colidx)"
+        @click="onClickTd(rowidx, colidx)"
+        @contextmenu.prevent="onRightClickTd(rowidx, colidx)"
+      >
+              {{colDataText(rowidx,colidx)}}
           </td>
       </tr>
-  </div>
+  </table>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import { MineSpace , CODE, FlagSpace , NormalSpace , OpenSpace , QuestionSpace  } from '../store/index';
 export default {
     computed:{
-        ...mapState(['tabledata'])
+        ...mapState(['tabledata','halted']),
+        //코드를 검사해서 코드마다 스타일을 다르게 적용
+        colDataStyle(state) {
+        return (row, col) => {
+          switch (this.$store.state.tabledata[row][col]) {
+            case CODE.Normal:
+            case CODE.Mine:
+              return {
+                background: '#444',
+              };
+            case CODE.ClickMine:
+            case CODE.Open:
+              return {
+                background: 'white',
+              };
+            case CODE.Flag:
+            case CODE.FlagOnMine:
+              return {
+                background: 'red',
+              };
+            case CODE.Question:
+            case CODE.QuestionOnMine:
+              return {
+                background: 'yellow',
+              };
+            default:
+              return {};
+          }
+        };
+      },
+      colDataText() {
+        return (row, col) => {
+          switch (this.$store.state.tabledata[row][col]) {
+            case CODE.Mine:
+              return 'X';
+            case CODE.Normal:
+              return '';
+            case CODE.FlagOnMine:
+            case CODE.Flag:
+              return '!';
+            case CODE.QuestionOnMine:
+            case CODE.Question:
+              return '?';
+            case CODE.ClickMine:
+              return '펑';
+            default:
+              return this.$store.state.tabledata[row][col] || '';
+          }
+        };
+      },
+    },
+methods: {
+      onClickTd(row, col) {
+        if (this.halted) {
+          return;
+        }
+        switch (this.tabledata[row][col]) {
+          case CODE.Normal:
+            return this.$store.commit(OpenSpace, { row, col });
+          case CODE.Mine:
+            return this.$store.commit(MineSpace, { row, col });
+          default:
+            return;
+        }
+      },
+      onRightClickTd(row, col) {
+        if (this.halted) {
+          return;
+        }
+        console.log(row, col);
+        switch (this.tabledata[row][col]) {
+          case CODE.Normal:
+          case CODE.Mine:
+            this.$store.commit(FlagSpace, { row, col });
+            return;
+          case CODE.FlagOnMine:
+          case CODE.Flag:
+            this.$store.commit(QuestionSpace, { row, col });
+            return;
+          case CODE.QuestionOnMine:
+          case CODE.Question:
+            this.$store.commit(NormalSpace, { row, col });
+            return;
+          default:
+            return;
+        }
+      }
     }
-}
+  };
+
 </script>
 
 <style>
--- p/src/views/Home.vue
+++ p/src/views/Home.vue
@@ -26,3 +26,16 @@ export default {
     },
 }
 </script>
+
+
+<style>
+table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
+</style>
