fbaa5affa56ec50bb1c9b5f79b56bc3e73154260
rlapo@naver.com <50164136+rlapo213@users.noreply.github.com>
2020-05-27 23:14:07 +0900

    updated socketIO

--- p/src/main.js
+++ p/src/main.js
@@ -2,8 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from "socket.io-client"
 
 Vue.config.productionTip = false
+
+const options = { path: '/my-app/' }; //Options object to pass into SocketIO
+
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: SocketIO('#', options), //options object is Optional
+    vuex: {
+      store,
+      actionPrefix: "SOCKET_",
+      mutationPrefix: "SOCKET_"
+    }
+  })
+);
 
 new Vue({
   router,
